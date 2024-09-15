af4e48c544e54c41553910d83efa6be199583d75
omarsafayet05 <safayetkhan4431@gmail.com>
2024-09-15 22:35:34 +0600

    Create the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg">
+      <div className="max-w-6xl p-3 mx-auto flex justify-between items-center">
+        {/* logo section */}
+        <Link
+          href={"/"}
+          className="text-2xl font-extrabold group cursor-pointer"
+        >
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600">
+            NEXT
+          </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600">
+            auth
+          </span>
+        </Link>
+        {/* navigation section */}
+        <nav>
+          <ul className="flex gap-4">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/sign-in"}>Sign in</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
